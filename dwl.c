5e7fe5683ef2faa126afd45550b63e3000e158f7
Carl <1953433+Carl-Git-hub@users.noreply.github.com>
2023-09-05 16:02:06 +1000

    Apply autostart patch

--- dwl.c
+++ dwl.c
@@ -240,6 +240,7 @@ static void arrangelayer(Monitor *m, struct wl_list *list,
 		struct wlr_box *usable_area, int exclusive);
 static void arrangelayers(Monitor *m);
 static void attachclients(Monitor *m);
+static void autostartexec(void);
 static void axisnotify(struct wl_listener *listener, void *data);
 static void buttonpress(struct wl_listener *listener, void *data);
 static void chvt(const Arg *arg);
@@ -416,6 +417,9 @@ static Atom netatom[NetLast];
 /* attempt to encapsulate suck into one file */
 #include "client.h"
 
+static pid_t *autostart_pids;
+static size_t autostart_len;
+
 /* function implementations */
 void
 applybounds(Client *c, struct wlr_box *bbox)
@@ -567,6 +571,27 @@ attachclients(Monitor *m)
 }
 
 void
+autostartexec(void) {
+	const char *const *p;
+	size_t i = 0;
+
+	/* count entries */
+	for (p = autostart; *p; autostart_len++, p++)
+		while (*++p);
+
+	autostart_pids = calloc(autostart_len, sizeof(pid_t));
+	for (p = autostart; *p; i++, p++) {
+		if ((autostart_pids[i] = fork()) == 0) {
+			setsid();
+			execvp(*p, (char *const *)p);
+			die("dwl: execvp %s:", *p);
+		}
+		/* skip arguments */
+		while (*++p);
+	}
+}
+
+void
 axisnotify(struct wl_listener *listener, void *data)
 {
 	/* This event is forwarded by the cursor when a pointer emits an axis event,
@@ -662,10 +687,20 @@ checkidleinhibitor(struct wlr_surface *exclude)
 void
 cleanup(void)
 {
+	size_t i;
 #ifdef XWAYLAND
 	wlr_xwayland_destroy(xwayland);
 #endif
 	wl_display_destroy_clients(dpy);
+
+	/* kill child processes */
+	for (i = 0; i < autostart_len; i++) {
+		if (0 < autostart_pids[i]) {
+			kill(autostart_pids[i], SIGTERM);
+			waitpid(autostart_pids[i], NULL, 0);
+		}
+	}
+
 	if (child_pid > 0) {
 		kill(child_pid, SIGTERM);
 		waitpid(child_pid, NULL, 0);
@@ -2098,6 +2133,7 @@ run(char *startup_cmd)
 		die("startup: backend_start");
 
 	/* Now that the socket exists and the backend is started, run the startup command */
+	autostartexec();
 	if (startup_cmd) {
 		int piperw[2];
 		if (pipe(piperw) < 0)
@@ -2504,8 +2540,8 @@ setup(void)
 void
 sigchld(int unused)
 {
-#ifdef XWAYLAND
 	siginfo_t in;
+	pid_t *p, *lim;
 	/* We should be able to remove this function in favor of a simple
 	 *	struct sigaction sa = {.sa_handler = SIG_IGN};
 	 * 	sigaction(SIGCHLD, &sa, NULL);
@@ -2516,11 +2552,22 @@ sigchld(int unused)
 	 * XWayland process
 	 */
 	while (!waitid(P_ALL, 0, &in, WEXITED|WNOHANG|WNOWAIT) && in.si_pid
-			&& (!xwayland || in.si_pid != xwayland->server->pid))
-		waitpid(in.si_pid, NULL, 0);
-#else
-	while (waitpid(-1, NULL, WNOHANG) > 0);
-#endif
+	#ifdef XWAYLAND
+		&& (!xwayland || in.si_pid != xwayland->server->pid)
+	#endif
+	)
+	waitpid(in.si_pid, NULL, 0);
+	if (in.si_pid == child_pid)
+	child_pid = -1;
+	if (!(p = autostart_pids))
+		return;
+	lim = &p[autostart_len];
+	for (; p < lim; p++) {
+		if (*p == in.si_pid) {
+			*p = -1;
+			break;
+		}
+	}
 }
 
 void
